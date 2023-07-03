5b46f2a6734eb2e95b6d3dd5062aa25cbc8c6a22
Mohammad Issa <issaa.mohammad12@gmail.com>
2023-07-04 00:47:58 +0300

    a

--- config/laratrust_seeder.php
+++ config/laratrust_seeder.php
@@ -35,6 +35,7 @@ return [
         'Purchasing-and-Sales-manager' => [
             'farms'=>'r,d',
             'note' =>'r,c',
+            'warehouse' => 'r',
             'statement after weight'=>'r'
         ],
 
