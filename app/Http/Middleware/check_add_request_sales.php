88e4f2039c964ac9f5c9c4cf4ba80eb0ee332231
masri <katiaalmasri2@gmail.com>
2023-07-24 10:31:28 +0300

    some editions

--- app/Http/Middleware/check_add_request_sales.php
+++ app/Http/Middleware/check_add_request_sales.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Middleware;
+
 use App\Models\outPut_Type_Production;
 use App\Models\Warehouse;
 use Closure;
@@ -20,23 +21,24 @@ class check_add_request_sales
      */
     public function handle(Request $request, Closure $next)
     {
-        if($request->request_type == 1){
-            foreach($request->details as $_detail){
-                $typaName = outPut_Type_Production::where('type',$_detail['type'])->get();
-                $warehouseContent = Warehouse::where('type_id',$typaName[0]->id)->with('outPut_Type_Production')->get()->first();
+        if ($request->request_type == 1) {
+            foreach ($request->details as $_detail) {
+                $typaName = outPut_Type_Production::where('type', $_detail['type'])->get();
+                $warehouseContent = Warehouse::where('type_id', $typaName[0]->id)->with('outPut_Type_Production')->get()->first();
 
                 $typeSalesRequest = DB::table('sales-purchasing-requset-details')
-                ->join('sales_purchasing_requests', 'sales_purchasing_requests.id', '=', 'sales-purchasing-requset-details.requset_id')
-                ->select('type', DB::raw('SUM(amount) as weight'))
-                ->where([['sales_purchasing_requests.accept_by_ceo',null],['type',$_detail['type']]])->groupBy('type')->get();
-
-                if($warehouseContent->tot_weight - $typeSalesRequest[0]->weight < 0 )
-                    return $this->returnError('error', 'عذرا الكمية في الانتظار');
-
-
+                    ->join('sales_purchasing_requests', 'sales_purchasing_requests.id', '=', 'sales-purchasing-requset-details.requset_id')
+                    ->select('type', DB::raw('SUM(amount) as weight'))
+                    ->where([['sales_purchasing_requests.accept_by_ceo', null], ['type', $_detail['type']]])->groupBy('type')->get();
+                if (count($typeSalesRequest) != 0) {
+                    if ($warehouseContent->tot_weight - $typeSalesRequest[0]->weight < 0)
+                        return $this->returnError('error', 'عذرا الكمية في الانتظار');
                 }
 
+
             }
+
+        }
 
         return $next($request);
     }
