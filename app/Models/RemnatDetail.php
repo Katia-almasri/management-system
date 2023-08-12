8b7c49ece86d9e54f32be0cea3c4ddbb49f02294
masri <katiaalmasri2@gmail.com>
2023-08-12 14:48:45 +0300

    date format editions

--- app/Models/RemnatDetail.php
+++ app/Models/RemnatDetail.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -21,4 +22,19 @@ class RemnatDetail extends Model
         return $this->belongsTo('App\Models\Output_remnat_details', 'output_remnat_det_id', 'id');
     }
     ############################## End Relations ##############################
+
+     ############################# Begin Accessors ##############################endregion
+     public function getCreatedAtAttribute($date)
+     {
+         if($date!=null)
+             return Carbon::parse($date)->format('Y-m-d H:i');
+         return $date;
+     }
+ 
+     public function getUpdatedAtAttribute($date)
+     {
+         if($date!=null)
+             return Carbon::parse($date)->format('Y-m-d H:i');
+         return $date;
+     }
 }
--- app/Models/commandSalesDetail.php
+++ app/Models/commandSalesDetail.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -30,5 +31,20 @@ class commandSalesDetail extends Model
     }
 
     ############################## End Relations ##############################
+
+     ############################# Begin Accessors ##############################endregion
+     public function getCreatedAtAttribute($date)
+     {
+         if($date!=null)
+             return Carbon::parse($date)->format('Y-m-d H:i');
+         return $date;
+     }
+ 
+     public function getUpdatedAtAttribute($date)
+     {
+         if($date!=null)
+             return Carbon::parse($date)->format('Y-m-d H:i');
+         return $date;
+     }
 
 }
